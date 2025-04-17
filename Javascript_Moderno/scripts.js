56e809a990d79abf74c967326b2d7ccd176086b1
RyanLudieri <ryan.evangelista@sou.unifal-mg.edu.br>
2025-04-17 17:49:10 -0300

    feat: adding examples of the class related to js classes

--- Javascript_Moderno/scripts.js
+++ Javascript_Moderno/scripts.js
@@ -180,4 +180,29 @@ const car = { ...carName, ...carBrand, ...otherInfos, wheels: 4}
 
 console.log(car)
 
+// 8 classe
+class Product{
+    constructor(name, price){
+        this.name = name
+        this.price = price
+    }
+
+    productWithDiscount(discount){
+        return this.price * ((100 - discount) / 100)
+    }
+}
+
+const shirt = new Product('Camisa gola v', 20)
+
+console.log(shirt.name)
+
+console.log(shirt. productWithDiscount(10))
+
+console.log(shirt. productWithDiscount(50))
+
+const tenis = new Product("Tenis verde", 120)
+
+console.log(tenis.productWithDiscount(20))
+
+
 
