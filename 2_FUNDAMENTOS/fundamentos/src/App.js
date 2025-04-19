f58ca5c493f25624f792a8552ac5f36d9f0dee71
RyanLudieri <ryan.evangelista@sou.unifal-mg.edu.br>
2025-04-19 17:01:30 -0300

    feat: importing the component

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+//styles / CSS
 import './App.css';
+
+// components
+import FirstComponent from './components/FirstComponent'
 
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
+      <FirstComponent/>
     </div>
   );
 }
