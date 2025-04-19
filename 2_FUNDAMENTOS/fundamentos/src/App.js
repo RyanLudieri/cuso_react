337a1121c4caf94de9dcb7328e16fd588ce8aa72
RyanLudieri <ryan.evangelista@sou.unifal-mg.edu.br>
2025-04-19 17:21:37 -0300

    feat:adding examples from the Template Expressions related class

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 // components
 import FirstComponent from './components/FirstComponent'
+import TemplateExpressions from './components/TemplateExxpressions';
 
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
       <FirstComponent/>
+      <TemplateExpressions/>
     </div>
   );
 }
--- 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -1,6 +1,16 @@
+// arquivo de estilo
+
+
 const FirstComponent = () =>{
+    //essa função é o primeiro componente
+
     return(
-        <div>Meu primeiro componente</div>
+        <div>
+            {/*Algum comentario */}
+            <h1>Meu primeiro componente</h1>
+            <p className="teste">Meu texto</p>
+        </div>
+        
     );
 };
 
