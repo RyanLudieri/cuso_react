fd9c3f0b615a73f96af10b1e6d6eeb7ba868ec03
RyanLudieri <ryan.evangelista@sou.unifal-mg.edu.br>
2025-04-23 10:50:37 -0300

    feat: adding examples from the previous state class

--- 3_AVANCANDO_EM_REACT/avancando_em_react/src/components/ListRender.js
+++ 3_AVANCANDO_EM_REACT/avancando_em_react/src/components/ListRender.js
@@ -4,11 +4,20 @@ const ListRender = () => {
 
   const [list] = useState(["Matheus", "Ryan", "Léticia", "Julia"])
 
-  const [users] = useState([
+  const [users, setUsers] = useState([
     {id: 1, name: "Ryan", age: 21},
-    {id: 815611, name: "Sebastião", age: 70},
-    {id: 8783021, name: "Rafael", age: 40},
+    {id: 2, name: "Sebastião", age: 70},
+    {id: 3, name: "Rafael", age: 40},
   ]);
+
+  const deleteRandom = () => {
+    const randomNumber = Math.floor(Math.random() * 4)
+
+    setUsers((prevUsers) => {
+      console.log(prevUsers)
+      return prevUsers.filter(((user) => randomNumber !== user.id))
+    });
+  }
 
   return (
     <div>
@@ -24,6 +33,7 @@ const ListRender = () => {
                 </li>
             ))}
         </ul>
+        <button onClick={deleteRandom}>Delete Random</button>
     </div>
   )
 }
