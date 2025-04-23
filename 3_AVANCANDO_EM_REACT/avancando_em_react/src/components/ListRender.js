3a7ea505ad94278c99166d95bc485c924d12eb06
RyanLudieri <ryan.evangelista@sou.unifal-mg.edu.br>
2025-04-22 22:20:23 -0300

    fix: solving the key property problem

--- 3_AVANCANDO_EM_REACT/avancando_em_react/src/components/ListRender.js
+++ 3_AVANCANDO_EM_REACT/avancando_em_react/src/components/ListRender.js
@@ -1,14 +1,27 @@
-import {useState} from 'react'
+import {use, useState} from 'react'
 
 const ListRender = () => {
 
   const [list] = useState(["Matheus", "Ryan", "Léticia", "Julia"])
 
+  const [users] = useState([
+    {id: 1, name: "Ryan", age: 21},
+    {id: 815611, name: "Sebastião", age: 70},
+    {id: 8783021, name: "Rafael", age: 40},
+  ]);
+
   return (
     <div>
         <ul>
-            {list.map((item) => (
-                <li>{item}</li>
+            {list.map((item, i) => (
+                <li key={i}>{item}</li>
+            ))}
+        </ul>
+        <ul>
+            {users.map((user) => (
+                <li key={user.id}>
+                    {user.name} - {user.age}
+                </li>
             ))}
         </ul>
     </div>
