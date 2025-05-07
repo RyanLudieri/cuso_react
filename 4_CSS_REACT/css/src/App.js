b24267e3b42038a9a30f35b07d8caca3da07b02d
RyanLudieri <ryan.evangelista@sou.unifal-mg.edu.br>
2025-05-07 18:34:57 -0300

    feat: examples from the class regarding inline CSS

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -2,7 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import MyComponent from './components/MyComponent';
 
+import {useState} from "react"
+
 function App() {
+
+  const m = 15;
+  const [name] = useState("Matheus")
+
   return (
     <div className="App">
       {/*Css global */}
@@ -10,6 +16,25 @@ function App() {
       {/*Css componente*/}
       <MyComponent/>
       <p>Este parágrafo é do App.js</p>
+      {/*Inline CSS*/}
+      <p style={{color: "magenta", padding: "25px", borderTop: "2px solid red"}}>
+        Este elemento foi estilizado de forma inline
+      </p>
+      <p style={{color: "magenta", padding: "25px", borderTop: "2px solid red"}}>
+        Este elemento foi estilizado de forma inline
+      </p>
+      {/*CSS Inline dinâmico */}
+      <h2 style={m < 10 ? ({color: "purple"}) : ({color: "pink"})}>
+        CSS dinamico
+      </h2>
+      <h2 style={m > 10 ? ({color: "purple"}) : ({color: "pink"})}>
+        CSS dinamico
+      </h2>
+      <h2 style={name == "Matheus" 
+        ? ({color: "green",  backgroundColor: "#000"}) 
+        : null}>
+        Teste nome
+      </h2>
     </div>
   );
 }
