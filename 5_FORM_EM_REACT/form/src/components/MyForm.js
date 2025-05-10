c9531ae34d2e20c76c7b82bcfc07be6535ab9bf7
RyanLudieri <ryan.evangelista@sou.unifal-mg.edu.br>
2025-05-09 22:08:50 -0300

    feat: examples of the label class involving input

--- 5_FORM_EM_REACT/form/src/components/MyForm.js
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -9,6 +9,11 @@ const MyForm = () => {
                 <label htmlFor="name">Name:</label>
                 <input type="text" name="name" placeholder='Digite o seu nome'/>
             </div>
+            {/*label envolvendo input */}
+            <label>
+                <span>E-mail</span>
+                <input type="email" name='email' placeholder='Digite o seu e-mail' />
+            </label>
             <input type="submit" value="Enviar"/>
         </form>
     </div>
