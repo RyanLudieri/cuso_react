9b97cf57d8000c1f4b656a4a56becc14296834b5
RyanLudieri <ryan.evangelista@sou.unifal-mg.edu.br>
2025-05-09 21:54:57 -0300

    feat: examples from the form creation class

---
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -0,0 +1,18 @@
+import './MyForm.css'
+
+const MyForm = () => {
+  return (
+    <div>
+        {/*Criação de form */}
+        <form>
+            <div>
+                <label htmlFor="name">Name:</label>
+                <input type="text" name="name" placeholder='Digite o seu nome'/>
+            </div>
+            <input type="submit" value="Enviar"/>
+        </form>
+    </div>
+  )
+}
+
+export default MyForm
