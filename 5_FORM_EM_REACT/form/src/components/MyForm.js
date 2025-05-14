63582c62df147aff20862cdbb8ee6ffb697ff6f9
RyanLudieri <ryan.evangelista@sou.unifal-mg.edu.br>
2025-05-13 21:48:11 -0300

    feat: adding examples of select related classes

--- 5_FORM_EM_REACT/form/src/components/MyForm.js
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -8,7 +8,8 @@ const MyForm = ({user}) => {
   const [name,setName] = useState(user ? user.name : '');
   const [email,setEmail] = useState(user ? user.email : '')
 
-  const [bio, setBio] = useState("")
+  const [bio, setBio] = useState(user ? user.bio : '')
+  const [role, setRole] = useState(user ? user.role : '')
   
   const handleName = (e) => {
     setName(e.target.value)
@@ -20,7 +21,7 @@ const MyForm = ({user}) => {
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log("Enviando o formulário");
-    console.log(name, email,bio);
+    console.log(name, email,bio, role);
 
     // 7 - limpar form
     setName("");
@@ -64,6 +65,19 @@ const MyForm = ({user}) => {
                 onChange={(e) => setBio(e.target.value)} 
                 value={bio}
                 ></textarea>
+            </label>
+            {/* 9 - select */}
+            <label>
+              <span>Função no sistema</span>
+              <select 
+                name="role" 
+                onChange={(e) => setRole(e.target.value)} 
+                value={role}
+              >
+                <option value="user">Usuário</option>
+                <option value="editor">Editor</option>
+                <option value="admin">Administrador</option>
+              </select>
             </label>
             <input type="submit" value="Enviar"/>
         </form>
