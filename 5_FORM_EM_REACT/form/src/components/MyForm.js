bee5c441a2d9d2d0ddad8ffabbd20aa68fc830c0
RyanLudieri <ryan.evangelista@sou.unifal-mg.edu.br>
2025-05-13 21:39:20 -0300

    feat: adding examples of classes related to resetting forms and Textarea

--- 5_FORM_EM_REACT/form/src/components/MyForm.js
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -7,6 +7,8 @@ const MyForm = ({user}) => {
   // 3- gerenciamento de dados
   const [name,setName] = useState(user ? user.name : '');
   const [email,setEmail] = useState(user ? user.email : '')
+
+  const [bio, setBio] = useState("")
   
   const handleName = (e) => {
     setName(e.target.value)
@@ -18,7 +20,12 @@ const MyForm = ({user}) => {
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log("Enviando o formulário");
-    console.log(name, email);
+    console.log(name, email,bio);
+
+    // 7 - limpar form
+    setName("");
+    setEmail("");
+    setBio("")
   }
 
   return (
@@ -47,6 +54,16 @@ const MyForm = ({user}) => {
                     onChange={(e) => setEmail(e.target.value) }
                     value={email} 
                 />
+            </label>
+            {/* 8 -textarea */}
+            <label>
+              <span>Bio:</span>
+              <textarea 
+                name="bio" 
+                placeholder='Descrição do usuario' 
+                onChange={(e) => setBio(e.target.value)} 
+                value={bio}
+                ></textarea>
             </label>
             <input type="submit" value="Enviar"/>
         </form>
