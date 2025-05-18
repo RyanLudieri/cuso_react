39a363a21d782df6955137d81caf72ec102df40d
RyanLudieri <ryan.evangelista@sou.unifal-mg.edu.br>
2025-05-18 18:45:05 -0300

    feat: finishing secret word project

--- 6_SECRET_WORD/secretword/src/App.js
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -34,24 +34,25 @@ function App() {
   const [guesses, setGuesses] = useState(3)
   const [score, setScore] = useState(0)
 
-  const pickWordAndCategory = () => {
+  const pickWordAndCategory = useCallback(() => {
     // pick a random category
     const categories = Object.keys(words);
     const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
     
-    console.log(category);
 
     // pick a random word
     const word = words[category][Math.floor(Math.random() * words[category].length)];
 
-    console.log(word);
 
     return {word, category};
 
-  }
+  }, [words]);
 
   // starts the secret word game
-  const startGame = () => {
+  const startGame = useCallback(() => {
+    //clear all letters
+    clearLetterStates();
+
     // pick word and pick category
     const {word, category} = pickWordAndCategory();
 
@@ -60,9 +61,6 @@ function App() {
     
     wordLetters = wordLetters.map((l) => l.toLowerCase())
 
-    console.log(word, category)
-    console.log(wordLetters)
-
     //fill states
     setPickedWord(word)
     setPickedCategory(category)
@@ -70,7 +68,7 @@ function App() {
 
 
     setGameStage(stages[1].name);
-  }
+  }, [pickWordAndCategory]);
 
   // process the letter input
   const verifyLetter = (letter) => {
@@ -105,6 +103,7 @@ function App() {
     setWrongLetters([]);
   }
 
+  // check if guesses ended
   useEffect(() => {
     if(guesses <= 0){
       // reset all stages
@@ -113,6 +112,21 @@ function App() {
       setGameStage(stages[2].name)
     }
   }, [guesses]);
+
+  // check win condition
+  useEffect(() => {
+    
+    const uniqueLetters = [...new Set(letters)]
+
+    // win condition
+    if(guessedLetters.length === uniqueLetters.length){
+      // add score
+      setScore((actualScore) => actualScore += 100)
+
+      //restart game with new word
+      startGame();
+    }
+  },[guessedLetters, letters, startGame])
 
   const retry = () => {
     setScore(0);
