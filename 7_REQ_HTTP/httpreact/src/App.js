e24798087e564219b6aa009b1e4e1bb7da88c31d
RyanLudieri <ryan.evangelista@sou.unifal-mg.edu.br>
2025-05-21 12:17:51 -0300

    feat: retrieving data from the API

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -1,9 +1,44 @@
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
+const url = "http://localhost:3000/products";
+
 function App() {
-  <div className="App">
-    <h1>Lista de Produtos</h1>
-  </div>
+  
+  const [products, setProducts] = useState([]);
+  
+  // 1 - resgatando dados
+  useEffect(() => {
+  const fetchData = async () => {
+    try {
+
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+
+    } catch (error) {
+      console.error("Erro ao buscar os produtos:", error);
+    }
+  };
+
+  fetchData();
+}, []);
+
+
+
+  return( 
+    <div className="App">
+      <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} - R$: {product.price}</li>
+        ))}
+      </ul>
+   </div>
+  );
 }
 
 export default App;
