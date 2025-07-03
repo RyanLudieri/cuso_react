7d678a92c6161be6e84a5d1308f82a5ad4ca542d
RyanLudieri <ryan.evangelista@sou.unifal-mg.edu.br>
2025-07-03 20:08:51 -0300

    feat: changing more complex context

--- 9_CONTEXT/context/src/pages/Home.js
+++ 9_CONTEXT/context/src/pages/Home.js
@@ -15,7 +15,12 @@ const Home = () => {
   const {counter} = useCounterContext();
 
   // 5 - context mais complexo
-  const {color} = useTitleColorContext();
+  const {color,dispatch} = useTitleColorContext();
+
+  //6 - alterando state complexo
+  const setTitleColor = (color) => {
+    dispatch({ type: color })
+  }
 
   return (
     <div>
@@ -23,6 +28,11 @@ const Home = () => {
       <p>Valor do contador: {counter}</p>
       {/*3 - alterando valor contexto */}
       <ChangeCounter />
+      {/* 6 -alterando contexto complexo*/}
+      <div>
+        <button onClick={() => setTitleColor("RED")}>Vermelho</button>
+        <button onClick={() => setTitleColor("BLUE")}>Azul</button>
+      </div>
     </div>
   )
 }
