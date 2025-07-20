f8b27a582b4a590ce406a41e4e56263869e9238b
RyanLudieri <ryan.evangelista@sou.unifal-mg.edu.br>
2025-07-20 20:58:32 -0300

    feat: add CSS navBar and footer

--- 10_MINIBLOG/miniblog/src/components/Footer.js
+++ 10_MINIBLOG/miniblog/src/components/Footer.js
@@ -1,10 +1,12 @@
 import styles from "./Footer.module.css"
 
 const Footer = () => {
-  return <footer>
-    <h3>Escreva sobre o que você tem interesse!</h3>
-    <p>Mini Blog &copy; 2025 </p>
-  </footer>
+  return (
+    <footer className={styles.footer}> 
+        <h3>Escreva sobre o que você tem interesse!</h3>
+        <p>Mini Blog &copy; 2025 </p>
+    </footer>
+  )
 }
 
 export default Footer
